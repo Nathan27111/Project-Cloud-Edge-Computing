f9c7d479c328eb6932e933020cbcbd0c72e38b3d
david.laridon <david.laridon@student.howest.be>
2021-11-26 21:27:36 +0100

    adds tournament bracket component

---
+++ src/components/TournamentBracketElement.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+    class="
+      w-5/6
+      h-auto
+      text-center
+      mx-auto
+      flex flex-col
+      bg-primary-light
+      p-3
+      rounded-sm
+      shadow-lg
+      mb-3
+    "
+  >
+    <h2 class="text-center">Table 1</h2>
+
+    <div class="grid grid-flow-col grid-cols-3">
+      <h3 class="overflow-hidden">Nathan</h3>
+      <div class="flex justify-evenly">
+        <div class="score-player-1">
+            <p>5</p>
+        </div>
+        <div class="score-player-2">
+            <p>1</p>
+        </div>
+      </div>
+      <h3 class="overflow-hidden">David</h3>
+    </div>
+
+    <div class="grid grid-flow-col grid-cols-3">
+      <div class="faults-player-1">
+        <ul class="flex justify-center">
+          <li>x</li>
+          <li>x</li>
+          <li>o</li>
+        </ul>
+      </div>
+      <h2>Not started</h2>
+      <div class="faults-player-2">
+        <ul class="flex justify-center">
+          <li>x</li>
+          <li>o</li>
+          <li>o</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="flex flex-col justify-evenly items-center mt-16">
     <h1 class="title">Airhockey Tournament</h1>
+    <div id="round-1" class="w-full">
+      <TournamentBracketElement />
+      <TournamentBracketElement />
+      <TournamentBracketElement />
+    </div>
+    <div id="round-2">
+
+    </div>
+    <div id="round-3">
+
+    </div>
   </div>
 </template>
 
 <script>
 import authRedirect from "../mixins/authRedirect";
 
+import TournamentBracketElement from "../components/TournamentBracketElement.vue";
+
 export default {
     mixins: [authRedirect],
+
+    components: {
+      TournamentBracketElement,
+    },
 };
 </script>
