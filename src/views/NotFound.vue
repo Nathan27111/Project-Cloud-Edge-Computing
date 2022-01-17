3e1a92577e1b41a6955604c0c3989ef5b72d71c6
david.laridon <david.laridon@student.howest.be>
2022-01-17 10:31:10 +0100

    David: adds styling 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Oops, it looks like the page you're looking for doesn't exist.</h1>
+  <div class="max-w-xs mx-auto text-center mt-56">
+    <h1 class="mb-5">Oops, it looks like the page you're looking for doesn't exist.</h1>
+    <button class="btn-small" @click="goHome">Go Home</button>
+  </div>  
 </template>
+
+<script>
+export default {
+  methods: {
+    goHome() {
+      this.$router.push("/");
+    }
+  }
+}
+</script>
